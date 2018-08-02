bb1f60d83013e390eee5a01eefe1f15d7da28e85
Hugo Silva <hugovictorsilva94@gmail.com>
2018-08-02 18:48:08 -0300

    Adição da implementação em Python da Regra do Ponto Médio

---
+++ rpm.py
@@ -0,0 +1,30 @@
+#Aqui é declarado a função a ser resolvida
+def func (x):
+    f = 1/x
+    return f
+#Aqui é onde acontece tudo, "a" e "b" são os intervalos definidos e "n" é o número de divisões
+def integral(a, b, n):
+    #Calculo do delta x
+    dx = (b-a)/n
+    #Declaração da lista que irá receber os valores dos pontos médios
+    lista = []
+    #"pm1" é o primeiro ponto médio encontrado... ele é tratado agora para não dar dor de cabeça depois
+    pm1 = (dx / 2) + a
+    #"pm1" é inserido na lista
+    lista.append(pm1)
+    #"soma" será a variável responsável pelo somatório de todos os pontos médios na f(x)
+    #"func(pm1)" indica que o valor recebido por "soma" já é o resultado do calculo do "pm1" na função
+    soma = func(pm1)
+    #Aqui "i" é incializado em 1 por causa desse "1" antes do "n" em "range(1,n)"
+    for i in range(1, n):
+        #Aqui é adicionado na lista os pontos médios
+        lista.append(lista[i-1]+dx)
+        #Aqui é adicionado na soma os valores dos pontos seguintes aplicados na função definida
+        soma +=func(lista[i])
+    #"result" recebe a multiplicação de delta x com a soma de todos os pontos médios já aplicados na função
+    result = dx*soma
+    #Enfeites de impressão
+    print("lista =", lista)
+    print("soma =", soma)
+    print("resultado =", result)
+integral(1, 2, 5)
